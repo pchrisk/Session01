5a8015044b47d1dbe666820102941333dc776090
pchrisk <chriskindelberger@hotmail.com>
2015-02-05 15:18:25 -0800

    20150205_318

--- KindelbergerC03/src/Library.java
+++ KindelbergerC03/src/Library.java
@@ -1,4 +1,33 @@
+// Add the appropriate imports
 
-public class Library {
+/**
+*
+* @author appleman
+*/
+class Library {
 
+  /**
+  * All checked out Books are stored in this map. The HashMap 
+  * has a String key for the isbn and the data is a String 
+  * with patron's name
+  */
+  HashMap checkoutBooks; // complete this declaraion using generics
+
+  /**
+  * Constructor, initializes the entries HashMap.
+  */
+  Library() {
+
+  // perform initialization here
+
+  }
+
+  /**
+  * add isbn book checked out to patron 
+  */
+  public void checkoutBook(String isbn, String patron) {
+
+  // add your code here
+
+  } 
 }
