0ff8cbb0339b7e79d85f10cc5304691cba3849d9
pchrisk <chriskindelberger@hotmail.com>
2015-02-08 15:20:50 -0800

    20150208_1520

--- KindelbergerC03/src/Library.java
+++ KindelbergerC03/src/Library.java
@@ -3,8 +3,6 @@ import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.Set;
 
-// Add the appropriate imports
-
 /**
  *
  * @author Chris Kindelberger
@@ -16,18 +14,14 @@ class Library {
 	 * All checked out Books are stored in this map. The HashMap has a String
 	 * key for the isbn and the data is a String with patron's name
 	 */
-	 HashMap<String, ArrayList<String>> checkoutBooks; // complete this
-	// declaraion using generics
+	 HashMap<String, ArrayList<String>> checkoutBooks; // complete this declaraion using generics
 	
 	/**
 	 * Constructor, initializes the entries HashMap.
 	 */
 	Library() {
-
-		// perform initialization here
-
-		checkoutBooks = new HashMap<String, ArrayList<String>>();
-
+		
+		checkoutBooks = new HashMap<String, ArrayList<String>>();// perform initialization here
 	}
 
 	/**
@@ -35,8 +29,6 @@ class Library {
 	 */
 	public void checkoutBook(String isbn, String patron) {
 
-		// add your code here
-			
 		ArrayList<String> patronList = checkoutBooks.get(isbn);
 		
 	    // if list does not exist create it
@@ -48,7 +40,6 @@ class Library {
 	        // add if item is not already in list
 	        if(!patronList.contains(patron)) patronList.add(patron);
 	    }
-
 	}
 	
 	public void checkoutBook(String isbn, String... patron ) { //variable argument list
@@ -61,24 +52,21 @@ class Library {
 		ArrayList<String> patronList = checkoutBooks.get(isbn);
 		ListIterator<String> iterator = patronList.listIterator();
 		System.out.println("ISBN:\t"+isbn);
-		//System.out.println("\t\t"+iterator);
 		while (iterator.hasNext())
 			System.out.println("   " + iterator.next());
-
 	}
 
 	public void returnBook(String isbn, String patron) {
 		ArrayList<String> patronList = checkoutBooks.get(isbn);
 		if (patronList.contains(patron)) {
 			patronList.remove(patron);
+			checkoutBooks.replace(isbn, patronList);
 			System.out.println("Book "+ isbn +": returned by " + patron);
 		} else {
 			System.out.println(patron +" has not checkout book " + isbn);
 		}
-		
-		checkoutBooks.replace(isbn, patronList);
-
 	}
+	
 	public void printAllCheckoutBooks() {
 		Set<String> isbnList = checkoutBooks.keySet();
 		for(String isbn: isbnList) {
