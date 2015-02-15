b65767eb4c6aa4a4ab62b552ad311d4d67238d08
pchrisk <chriskindelberger@hotmail.com>
2015-02-14 23:11:45 -0800

    20150214_2311

--- LastFM04/src/edu/washington/ext/Book.java
+++ LastFM04/src/edu/washington/ext/Book.java
@@ -3,6 +3,7 @@ package edu.washington.ext;
 import java.util.HashSet;
 import java.util.Set;
 
+// TODO: Auto-generated Javadoc
 /**
  * A class that maintains information on a book. This might form part of a
  * larger application such as a library system, for instance.
@@ -14,18 +15,38 @@ import java.util.Set;
 class Book {
 
 	// The fields.
+	/** The author. */
 	private String author = null;
+	
+	/** The title. */
 	private String title = null;
+	
+	/** The publisher. */
 	private String publisher = null;
+	
+	/** The binding. */
 	private Binding binding;
+	
+	/** The pages. */
 	private int pages = 0;
+	
+	/** The ref number. */
 	private String refNumber = null;
+	
+	/** The description. */
 	private String description = null; //description of the Book
 	
 
 	/**
 	 * Constructor. Set the author, title and binding fields when this object is
 	 * constructed.
+	 *
+	 * @param bookAuthor the book author
+	 * @param bookTitle the book title
+	 * @param bookPublisher the book publisher
+	 * @param bindingType the binding type
+	 * @param bookPages the book pages
+	 * @param bookDescription the book description
 	 */
 	Book(String bookAuthor, String bookTitle, String bookPublisher,
 			Binding bindingType, int bookPages, String bookDescription) {
@@ -39,56 +60,114 @@ class Book {
 		
 	}
 
+	/**
+	 * Gets the author.
+	 *
+	 * @return the author
+	 */
 	public String getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(String author) {
+	/**
+	 * Sets the author.
+	 *
+	 * @param author the new author
+	 */
+	public void setAuthor(final String author) {
 		this.author = author;
 	}
 
+	/**
+	 * Gets the title.
+	 *
+	 * @return the title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
-	public void setTitle(String title) {
+	/**
+	 * Sets the title.
+	 *
+	 * @param title the new title
+	 */
+	public void setTitle(final String title) {
 		this.title = title;
 	}
 
+	/**
+	 * Gets the publisher.
+	 *
+	 * @return the publisher
+	 */
 	public String getPublisher() {
 		return publisher;
 	}
 
-	public void setPublisher(String publisher) {
+	/**
+	 * Sets the publisher.
+	 *
+	 * @param publisher the new publisher
+	 */
+	public void setPublisher(final String publisher) {
 		this.publisher = publisher;
 	}
 
+	/**
+	 * Gets the binding.
+	 *
+	 * @return the binding
+	 */
 	public Binding getBinding() {
 		return binding;
 	}
 
-	public void setBinding(Binding binding) {
+	/**
+	 * Sets the binding.
+	 *
+	 * @param binding the new binding
+	 */
+	public void setBinding(final Binding binding) {
 		this.binding = binding;
 	}
 	
+	/**
+	 * Gets the pages.
+	 *
+	 * @return the pages
+	 */
 	public int getPages() {
 		return pages;
 	}
 
+	/**
+	 * Gets the ref number.
+	 *
+	 * @return the ref number
+	 */
 	public String getRefNumber() {
 		return refNumber;
 	}
 
-	public void setRefNumber(String refNumber) {
+	/**
+	 * Sets the ref number.
+	 *
+	 * @param refNumber the new ref number
+	 */
+	public void setRefNumber(final String refNumber) {
 		
 		if (refNumber.length() < 3) {
-			System.out.println("There was an error, the Reference Number must be 3 characters or more.");
+			System.out.println("There was an error, the Reference Number "
+					+ "must be 3 characters or more.");
 		} else {
 			this.refNumber = refNumber;
 		}
 	}
 	
 	/**
+	 * Gets the description.
+	 *
 	 * @return the description
 	 */
 	public String getDescription() {
@@ -96,32 +175,52 @@ class Book {
 	}
 
 	/**
+	 * Sets the description.
+	 *
 	 * @param description the description to set
 	 */
-	public void setDescription(String description) {
+	public void setDescription(final String description) {
 		this.description = description;
 	}
 
+	/**
+	 * Prints the author.
+	 */
 	public void printAuthor() {
 		System.out.println(getAuthor());
 	}
 	
+	/**
+	 * Prints the title.
+	 */
 	public void printTitle() {
 		System.out.println(getTitle());
 	}
 	
+	/**
+	 * Prints the publisher.
+	 */
 	public void printPublisher() {
 		System.out.println(getPublisher());
 	}
 	
+	/**
+	 * Prints the binding.
+	 */
 	public void printBinding() {		
 		System.out.println(binding.getBindingType());		
 	}
 	
+	/**
+	 * Prints the pages.
+	 */
 	public void printPages() {
 		System.out.println(getPages());
 	}
 	
+	/**
+	 * Prints the details.
+	 */
 	public void printDetails() {
 		System.out.print("Author: ");
 		printAuthor();
@@ -143,9 +242,12 @@ class Book {
 		System.out.print("Description: ");
 		System.out.println(description);
 	}
+	
 	/**
-	*method returns the total number of words in the description.
-	*/
+	 * method returns the total number of words in the description.
+	 *
+	 * @return the total words in description
+	 */
 	public int getTotalWordsInDescription() {
 		int totalWords = 0;
 		String[] descriptionSplit = description.replaceAll("\\p{P}", "").split("\\s");
@@ -154,9 +256,12 @@ class Book {
 		return totalWords;
 		
 	}
+	
 	/**
 	 * , returns the total number of unique words in the description. 
 	 * Tip - a HashSet may be useful for identifying the unique words.
+	 *
+	 * @return the total unique words in description
 	 */
 	public int getTotalUniqueWordsInDescription() {
 		int totalUnique = 0;
@@ -176,17 +281,20 @@ class Book {
 	 *  this method takes one string parameter consisting of one word to 
 	 *  be counted in the description. The return value consists of how may 
 	 *  times the specified word occurs in the Book's description.
+	 *
+	 * @param word the word
+	 * @return the total word in description
 	 */
-	public int getTotalWordInDescription(String word) {
+	public int getTotalWordInDescription(final String word) {
 		int totalInstances = 0;
 		String[] descriptionSplit = description.replaceAll("\\p{Punct}", "").split("\\s");
 		for (String x : descriptionSplit) {
-			if(x.toLowerCase().equals(word)){
+			if (x.toLowerCase().equals(word)) {
 				totalInstances++;
 			}
 			
 		}
-		return totalInstances ;
+		return totalInstances;
 		
 	}
 	
--- LastFM04/src/edu/washington/ext/BookApp.java
+++ LastFM04/src/edu/washington/ext/BookApp.java
@@ -1,27 +1,46 @@
 package edu.washington.ext;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BookApp.
+ */
 public class BookApp {
 	
+	/** The course text. */
 	static Book courseText;
 	
+	/** The author. */
 	static String author = "Cay Horstman";
+	
+	/** The title. */
 	static String title = "Core Java Vol 1, 9th Ed";
+	
+	/** The publisher. */
 	static String publisher = "Prentice Hall";
+	
+	/** The binding. */
 	static Binding binding = Binding.HARDCOVER;
+	
+	/** The pages. */
 	static int pages =  1234;
 	
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String inputDesciption = null;
 		String inputWord = null;
 		
-		if(args.length == 1){
+		if (args.length == 1) {
 			inputDesciption = args[0];			
-		}else if(args.length == 2){
+		} else if (args.length == 2) {
 			inputDesciption = args[0];
 			inputWord = args[1]; //optional argument, used for finding the number of occurance of the given word.			
-		}else{
+		} else {
 			System.out.println("This app requires two parameters. The first is the book description. \nThe second (optional)"
 					+ " parameter is a word that you would like to determine \nhow many times it is used in the description"
 					+ "\nUsage: BookApp \"Book Description\" \"word\"{optional}");
@@ -31,21 +50,37 @@ public class BookApp {
 		execute(inputDesciption, inputWord);		
 	}
 
-	private static void execute(String inputDesciption, String inputWord) {
-		createBook(inputDesciption);
+	/**
+	 * Execute.
+	 *
+	 * @param inputDesciption the input description
+	 * @param inputWord the input word
+	 */
+	private static void execute(final String inputDescription, final String inputWord) {
+		createBook(inputDescription);
 		printDetails(inputWord);
 	}
 
-	private static void printDetails(String inputWord) {
+	/**
+	 * Prints the details.
+	 *
+	 * @param inputWord the input word
+	 */
+	private static void printDetails(final String inputWord) {
 		courseText.printDetails();
 		System.out.println("Total number of words in description: " + courseText.getTotalWordsInDescription());
 		System.out.println("Total number of unique words in description: " + courseText.getTotalUniqueWordsInDescription());
-		if(inputWord != null){
+		if (inputWord != null) {
 		System.out.println("Number of occurances of the word \"" + inputWord + "\": " 
 				+ courseText.getTotalWordInDescription(inputWord));
 		}
 	}
 	
+	/**
+	 * Creates the book.
+	 *
+	 * @param description the description
+	 */
 	private static void createBook(String description) {
 		courseText = new Book(author, title, publisher, binding, pages,description);
 		
