3e94e3c55242737ae3fe0d731e76c82cf2b04b6d
pchrisk <chriskindelberger@hotmail.com>
2015-02-14 22:36:23 -0800

    20150214_2036

--- LastFM04/src/edu/washington/ext/BookApp.java
+++ LastFM04/src/edu/washington/ext/BookApp.java
@@ -4,6 +4,12 @@ public class BookApp {
 	
 	static Book courseText;
 	
+	static String author = "Cay Horstman";
+	static String title = "Core Java Vol 1, 9th Ed";
+	static String publisher = "Prentice Hall";
+	static Binding binding = Binding.HARDCOVER;
+	static int pages =  1234;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,23 +17,23 @@ public class BookApp {
 		String inputWord = null;
 		
 		if(args.length == 1){
-			inputDesciption = args[0];
-			createBook(inputDesciption);
-			printDetails(null);
-			
+			inputDesciption = args[0];			
 		}else if(args.length == 2){
 			inputDesciption = args[0];
-			inputWord = args[1]; //optional argument, used for finding the number of occurance of the given word.
-			createBook(inputDesciption);
-			printDetails(inputWord);
-			
+			inputWord = args[1]; //optional argument, used for finding the number of occurance of the given word.			
 		}else{
 			System.out.println("This app requires two parameters. The first is the book description. \nThe second (optional)"
 					+ " parameter is a word that you would like to determine \nhow many times it is used in the description"
 					+ "\nUsage: BookApp \"Book Description\" \"word\"{optional}");
+			System.exit(1);
 		}
 		
-		
+		execute(inputDesciption, inputWord);		
+	}
+
+	private static void execute(String inputDesciption, String inputWord) {
+		createBook(inputDesciption);
+		printDetails(inputWord);
 	}
 
 	private static void printDetails(String inputWord) {
@@ -41,7 +47,7 @@ public class BookApp {
 	}
 	
 	private static void createBook(String description) {
-		courseText = new Book("Cay Horstman", "Core Java Vol 1, 9th Ed", "Prentice Hall", Binding.HARDCOVER, 1234,description);
+		courseText = new Book(author, title, publisher, binding, pages,description);
 		
 	}
 	
