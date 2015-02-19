2b0d43d7084df8219efa751f052266009799cb27
pchrisk <chriskindelberger@hotmail.com>
2015-02-19 15:23:12 -0800

    20150219_1523

--- KindelbergerCmid/src/edu/washington/midcourse/Main.java
+++ KindelbergerCmid/src/edu/washington/midcourse/Main.java
@@ -1,5 +1,15 @@
 package edu.washington.midcourse;
 
+import edu.washington.midcourse.util.Rot13;;
+
 public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Rot13.encrypt("A"));
+		
+		
+	}
 
 }
--- KindelbergerCmid/src/edu/washington/midcourse/util/Rot13.java
+++ KindelbergerCmid/src/edu/washington/midcourse/util/Rot13.java
@@ -1,5 +1,32 @@
 package edu.washington.midcourse.util;
 
 public class Rot13 {
+	
+	static char[] cc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+		
+	public static String encrypt(String encryptThis) {
+		String encyptedString = "False";
+		char[] ls = encryptThis.toCharArray();
+		char[] newls = new char[0];
+		for (int c = 0; c < ls.length; c++) {
+			for (int i = 0; i < cc.length; i++) {
+				if (ls[c] == cc[i]) {
+					int j = i + 13;
+					System.out.println(ls[c]);
+					System.out.println(cc[j]);
+					System.arraycopy(cc, j, newls, c, ls.length);//newls[c] = cc[j];
+				}
+				
+			}
+		}
+		encryptThis = newls.toString();
+		
+//		if (Character.isUpperCase(ls[0])) {
+//			encyptedString = "True";
+//		}
+			
+		return encyptedString;
+		
+	}
 
 }
