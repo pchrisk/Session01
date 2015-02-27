6d2a74bbe6e3f3cc6c4a59df5520ed3782150e84
pchrisk <chriskindelberger@hotmail.com>
2015-02-27 09:22:42 -0800

    20150227_0922

---
+++ KindelbergerC05/src/edu/washington/ext/Librarian.java
@@ -0,0 +1,24 @@
+package edu.washington.ext;
+
+public class Librarian implements Employee {
+	
+	private double bonusRate = 0;
+	private String name = null;
+	private double basePay = 0;
+	private double currentLibraryTotals = 0;
+	private double currentLibrarianTotals = 0;
+	
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calculatePay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ KindelbergerC05/src/edu/washington/ext/LibraryEmployee.java
@@ -0,0 +1,23 @@
+package edu.washington.ext;
+
+public class LibraryEmployee implements Employee {
+	
+	private double currentHours = 0;
+	private double hourlyRate = 0;
+	private String name = null;
+	private double currentSales = 0;
+	
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calculatePay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
